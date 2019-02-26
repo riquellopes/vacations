a3f8e6748bfc04201075e89f3abfe6dcc20d4983
Henrique Lopes <riquellopes@gmail.com>
2019-02-25 23:31:31 -0300

    adding client and log

--- main.go
+++ main.go
@@ -16,20 +16,49 @@ type Server struct {
 
 // WhenDoYouStartYourVacation -
 func (s *Server) WhenDoYouStartYourVacation(c context.Context, req *protons.HenriqueMessage) (*protons.HenriqueMessage, error) {
+	log.Printf("Asking from gRPC: %s", req.GetMsg())
 	return &protons.HenriqueMessage{Msg: "01/03/2019"}, nil
 }
 
-func main() {
+func server() {
 	server, err := net.Listen("tcp", ":50051")
 
 	if err != nil {
-		log.Printf("Error to listen: %v", err)
-		return
+		log.Fatalf("Error to listen: %v", err)
 	}
+
+	defer server.Close()
 
 	grpcServer := grpc.NewServer()
 	protons.RegisterHenriqueVacationsServer(grpcServer, &Server{})
 
 	reflection.Register(grpcServer)
 	grpcServer.Serve(server)
+}
+
+func client() {
+	dial, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("Error to connect %v", err)
+	}
+
+	defer dial.Close()
+
+	client := protons.NewHenriqueVacationsClient(dial)
+
+	asking := &protons.HenriqueMessage{Msg: "When do you start your vacation?"}
+	r, err := client.WhenDoYouStartYourVacation(context.Background(), asking)
+
+	if err != nil {
+		log.Fatalf("Error to replay %v", err)
+	}
+
+	log.Printf("gRPC say: %s", r.GetMsg())
+}
+
+func main() {
+	go server()
+
+	client()
 }
